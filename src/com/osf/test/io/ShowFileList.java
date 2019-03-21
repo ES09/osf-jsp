b33842819fdac16a4204a67e2c59f4719cf3e646
ES09 <e.song925@gmail.com>
2019-03-21 17:49:30 +0900

    jsp 파일올리기 수업

---
+++ src/com/osf/test/io/ShowFileList.java
@@ -0,0 +1,23 @@
+package com.osf.test.io;
+
+import java.io.File;
+
+public class ShowFileList {
+
+	public static void main(String[] args) {
+		File tFile = new File("d:\\test");
+		File[] files = tFile.listFiles();
+		for(int i=0;i<files.length;i++) {
+			File f = files[i];
+			// System.out.println(files[i].getName());
+			if(f.isDirectory()) {
+				File[] subFiles = f.listFiles();
+				for(int j=0;j<subFiles.length;j++) {
+					System.out.println(subFiles[j].getName());
+				}
+			} else {
+			System.out.println(files[i].getName());
+			}
+		}
+	}
+}
